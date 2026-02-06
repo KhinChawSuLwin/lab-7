80e05093ab9b1d8469885e0979cef2d5a3f849ff
680615521KhinChawSuLwin <khinchawsulwin161@gmail.com>
2026-02-06 16:02:26 +0700

    final completion

--- main.cpp
+++ main.cpp
@@ -1,19 +1,39 @@
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
 using namespace std;
 int main()
-{
-    list<int>lst;
-    for(int i=0; i<5; i++)
+{ 
+    int N,M;
+    cout << "Enter N "; //numbers of people
+    cin >> N;
+    cout << "Enter M "; //number of passes
+    cin >> M;
+    list<int> lst;
+    for(int i=1; i<=N; i++)
     {
         lst.push_back(i);
     }
 
     list<int>::iterator it=lst.begin();
 
-    cout << *it << endl;
-    it++;
-    cout << *it << endl;
+    cout << "Elimination order: ";
+    while(lst.size()>1)
+    {
+        for(int j=0; j<M; j++)
+        {
+            it++;
+            if(it==lst.end())
+                it=lst.begin(); //circular
 
+        }
+        cout << *it << " "; //eliminated person
+        //remove the person and get the next iterator
+        it=lst.erase(it);
+        //circular
+        if(it==lst.end())
+            it=lst.begin();
+    }
+    cout<<endl;
+    cout << "winner: " << lst.front() << endl;
     return 0;
 }
